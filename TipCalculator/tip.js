6db2bb7291fc73883427fb7bbe9620bc1d820756
John Dalton Lee-Cruz <jdleecruz@gmail.com>
2021-10-31 04:29:31 -0500

    Made a variety of changes/improvements.
    1. Made a complete design overhaul to make website look cleaner and easier to understand.
    2. Made significant improvements to responsiveness for mobile (portrait) devices.
    3. Added general comments to the code for readability.
    4. Changed certain id names and variable names for better understanding.

--- TipCalculator/tip.js
+++ TipCalculator/tip.js
@@ -1,60 +1,85 @@
-const range = document.getElementById("myRange");
-const number = document.querySelector(".value");
-const cost = document.getElementById("totalCost");
+//Cost and Tip variable declarations
+const cost = document.getElementById("costInput");
+const tip = document.getElementById("tipInput");
 
-const resultDiv = document.querySelector(".results");
-const evenDiv = document.querySelector(".even");
-
+//Button variable declarations
 const submit = document.getElementById("submitBtn");
 const reset = document.getElementById("resetBtn");
 const even = document.getElementById("evenBtn");
 
-number.innerHTML = range.value + "%";
+//Section 2 variable declaration
+const wrapper2Div = document.querySelector(".wrapper2");
 
-range.oninput = function() {
-    number.innerHTML = this.value + "%";
-}
-
+//Submit Button EventListener Function
+//When the user hits "submit" the following procedures occur.
+//1. Error checking to see if user entered correct values for
+//   both cost and tip textboxes.
+//2. General formatting of user input.
+//3. Required calculations to get the correct tip number based
+//   on entered percentage, and Final Total.
 submit.addEventListener("click", function() {
-    let resultCost = document.getElementById("costTotal");
+    //Variable declarations to be used for calculations.
+    let sub = document.getElementById("subTotal");
     let tipAmt = document.getElementById("tipAmount");
     let tipNum = document.getElementById("tipNumber");
     let endTotal = document.getElementById("newTotal");
 
+    //Sets "make even" button able to be pressed/useable.
     even.disabled = false;
 
-    if (isNaN(cost.value)) {
+    //The error checking for the user
+    //If the user enters a non-number, "abcde", an alert message will be displayed
+    //and we will exit the function.
+    if (isNaN(cost.value) || isNaN(tip.value)) {
         alert("Error: must input numbers only.");
         return false;
     }
+    //If the user enters nothing for the cost textbox an alert message will be displayed
+    //and we will exit the function.
     else if (cost.value === "") {
         alert("Error: please input a total cost.")
         return false;
     }
+    //If the user enters nothing for the tip textbox an alert message will be displayed
+    //and we will exit the function.
+    else if (tip.value === "") {
+        alert("Error: please input a tip percentage.")
+        return false;
+    }
 
+    //General formatting for cost and tip prompts.
     if (!cost.value.includes(".")) {
-        resultCost.innerHTML = cost.value + ".00$";
+        sub.innerHTML = cost.value + ".00$";
     }
     else {
-        resultCost.innerHTML = cost.value + "$";
+        sub.innerHTML = cost.value + "$";
     }
 
-    tipAmt.innerHTML = "(" + range.value + "%)";
+    //Will display the entered tip percentage in results section.
+    tipAmt.innerHTML = "(" + tip.value + "%)";
 
+    //General calculations to find the amount of tip money given and the resulting total
+    //subtotal + tip money = final total
     let total = parseFloat(cost.value);
-    let tip = parseInt(range.value) / 100;
-    let tipTotal = total * tip;
+    let tipDecimal = parseInt(tip.value) / 100;
+    let tipTotal = total * tipDecimal;
     let newTotal = total + tipTotal;
 
     tipNum.innerHTML = tipTotal.toFixed(2) + "$";
     endTotal.innerHTML = newTotal.toFixed(2) + "$";
 
+    //Make Even Button EventListener Function
+    //When pressed the previous calculations made beforehand will now
+    //make the final total an "even" cost
+    //ex) "12.78" --> "13.00"
+    //ex) "8.04" --> "8.00"
+    //This will alter the amount of tip money given and is accounted for and redisplayed as such.
     even.addEventListener("click", function() {
         let evenTotal = Math.round(newTotal);
         let remainder = evenTotal - newTotal;
         tipTotal = tipTotal + remainder;
 
-        tipAmt.innerHTML = "(~" + range.value + "%)";
+        tipAmt.innerHTML = "(~" + tip.value + "%)";
 
         tipNum.innerHTML = tipTotal.toFixed(2) + "$";
         endTotal.innerHTML = evenTotal.toFixed(2) + "$";
@@ -62,17 +87,17 @@ submit.addEventListener("click", function() {
         even.disabled = true;
     })
 
-    resultDiv.style.display = "block";
-    evenDiv.style.display = "block";
+    //Makes the second section visible with all the information.
+    wrapper2Div.style.display = "block";
 })
 
+//Reset Button EventListener Function
+//Sets all values back to the original state of when first loading the page.
 reset.addEventListener("click", function() {
     cost.value = "";
-    range.value = 50;
-    number.innerHTML = range.value + "%";
+    tip.value = "";
 
-    resultDiv.style.display = "none";
-    evenDiv.style.display = "none";
+    wrapper2Div.style.display = "none";
 
     even.disabled = false;
 })
